3bea65ab63d7dacf5ae124ff178c5a6684a04c78
bobtop42 <nv714774@gmail.com>
2025-09-28 20:58:52 -0700

    Create navsys.h

---
+++ navsys.h
@@ -0,0 +1,33 @@
+#include <cmath>
+#include "plane.h"
+#include "io.h"
+#ifndef NAVSYS_H
+#define NAVSYS_H
+
+//NOTE: add npc counter. Burrently only a hardcoded int
+
+class NAVSYS
+{
+public:
+PLANE *navsys = &plane;
+
+int npc = 1;
+
+const float PI = 3.14159f;
+
+void wayPointAngleFinder();
+void planeAngleFinder(CKALMAN* ckalman, PLANE* plane, HMC::HMC* hmc); //fill in with kalman filter stuff later
+void updateNpc(PLANE* plane);
+
+NAVSYS();
+
+private:
+int routeLen;
+float WPXYZ[14][3];
+
+void updateEP(PLANE& plane, float value);
+void updateAP(float value);
+
+};
+
+#endif
