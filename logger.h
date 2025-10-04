c823d79b28c8e801864500e3f4505892898119a7
bobtop42 <nv714774@gmail.com>
2025-10-03 17:14:29 -0700

    Create logger.h

---
+++ logger.h
@@ -0,0 +1,28 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+#include "plane.h"
+#include "io.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include<ctime>
+#include<iomanip>
+#include <tuple>
+
+class LOGGER
+{
+public:
+void log(PLANE* plane);
+void setFilename(std::string filetype);
+auto fillPlaneData(const std::string data, int logPos[67], PLANE* plane);
+auto parseLog(const std::string data, int logPos[67]);
+
+LOGGER(std::string file_):file(file_){}
+
+private:
+std::string file;
+
+};
+
+#endif
