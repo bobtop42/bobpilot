f13b96cdb8da8e1e8a5eaf955be0fe6337ea0709
bobtop42 <nv714774@gmail.com>
2025-10-03 18:36:33 -0700

    Update roll.h

--- roll.h
+++ roll.h
@@ -1,30 +1,36 @@
-#include "pid.h"
 #ifndef ROLL_H
 #define ROLL_H
+#include "pid.h"
+#include "MATHLIB.h"
+#include "plane.h"
 
 class ROLL
 {
 public:
+int8_t RollDir;
+/*
+if plane is straight RollDir -> 0x00
+if plane is rolling left RollDir -> 0xFF
+if plane is rolling right RollDir -> 0x01
+*/
 
-enum class Dir { Right, Straight, Left};
-Dir RollDir;
-
-DESCRETE RollPID;
+PID pid{0.1f, 0.1f, 0.1f};
 
 ROLL(float targetRoll, float currentRoll, bool engaged);
 
-void engaged(ROLL& myRoll);
-void disengaged(ROLL& myRoll);
-void update(DESCRETE* myDescrete);
+void targetRoll(float value, PLANE* plane);
+void targetRoll(PLANE* plane);
+void engaged();
+void disengaged();
+float update(PLANE* plane);
 void setUp();
+void adjustAileron(float value, PLANE* plane);
+
+float targetRoll_;
 
 private:
-float targetRoll_;
 float currentRoll_;
 bool engaged_;
-
-// add navsys stuff to parentheses
-void targetRoll();
 
 };
 
