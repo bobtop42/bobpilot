0f63251d5e6d0321c27b983ad86f694b6c9f8855
bobtop42 <nv714774@gmail.com>
2025-10-03 18:35:18 -0700

    Update roll.cpp

--- roll.cpp
+++ roll.cpp
@@ -1,67 +1,50 @@
 #include "roll.h"
 
-ROLL::ROLL(float targetRoll, float currentRoll, bool engaged):targetRoll_(targetRoll), currentRoll_(currentRoll), engaged_(engaged), RollPID(0.0f, 0.0f, 0.0f) {}
+ROLL::ROLL(float targetRoll, float currentRoll, bool engaged):targetRoll_(targetRoll), currentRoll_(currentRoll), engaged_(engaged) {}
 
-void ROLL::engaged(ROLL& myRoll)
+void ROLL::engaged()
 {
     engaged_ = true;
-    myRoll.targetRoll();
 }
 
-void ROLL::disengaged(ROLL& myRoll)
+void ROLL::disengaged()
 {
     engaged_ = false;
-    myRoll.targetRoll_ = 0.0f;
 }
 
-void ROLL::targetRoll()
+void ROLL::targetRoll(float value, PLANE* plane)
 {
-    float pa = 5.0f;  // placeholder for navsys.plane.PA.roll
-    float wpa = 1.0f; // placeholder for navsyz.plane.WPA.roll
-
-    if (wpa > pa && pa >= 0.0f)
-    {
-        // roll right & continue right
-        RollDir = Dir::Right;
-        targetRoll_ = wpa - pa;
-        return;
-    }
-    else if (wpa < pa && pa <= 0.0f)
-    {
-        // roll left & continue left
-        RollDir = Dir::Left;
-        targetRoll_ = pa - wpa;
-        return;
-    }
-    else if (wpa < pa && pa >= 0.0f)
-    {
-        // roll right & continue left
-        RollDir = Dir::Right;
-        targetRoll_ = pa - wpa;
-        return;
-    }
-    else if (wpa > pa && pa <= 0.0f)
-    {
-        // roll left & continue right
-        RollDir = Dir::Left;
-        targetRoll_ = wpa - pa;
-        return;
-    }
-    else
-    {
-        // NOTE: add flag raise if on course so
-        // user knows and/or something went wrong
-        // and defaults to this
-        targetRoll_ = 0.0f;
-        RollDir = Dir::Straight;
-    }
+    targetRoll_ = fabs(value);
+    RollDir = static_cast<int8_t>(value/fabs(value) + static_cast<float>(!static_cast<int>(fabs(value) + 0.9999999f)));
+    /*
+    returns 1 for rolling dir left, 0 for on course for roll, and -1 for rolling dir right.
+    code basicaly  does this:
+    value += 0.9999999; //makes sure any dec >=1, so when truncates non- zero goes to 1
+    static_cast -> int to truncate, then turns all non-zero vals to 0, and 0 -> 1, so when you div, no non-zero errors, then goes back to float, to div then int8_t for RollDir final value
+    */
 }
 
-void ROLL::update(DESCRETE* myDescrete)
+void ROLL::targetRoll(PLANE* plane)
+{
+    float pa = plane->PA.roll; 
+    float wp = plane->WPA.roll;
+
+    targetRoll_ = fabs(static_cast<float>(static_cast<int>(fabs(wp - pa)/6.28318f + 0.5f) * 6.28318f)) - fabs(wp - pa);//check last fabs if its in the first fabs parathesis
+}
+
+void ROLL::adjustAileron(float value, PLANE* plane)
+{
+    plane->ap.fL += value * static_cast<float>(RollDir) * -1.0f;
+    plane->ap.fR += value * static_cast<float>(RollDir) * -1.0f;
+    plane->ap.aileronAdj(plane->ap);
+}
+
+float ROLL::update(PLANE* plane)
 {
     if (engaged_)
     {
-        targetRoll();
-        myDescrete->calc(myDescrete, targetRoll_);
+        targetRoll(plane);
+        float pidAdj = pid.calculate(targetRoll_);
+        adjustAileron(pidAdj, plane);
     }
 }
