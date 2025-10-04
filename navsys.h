26b5f7d8fba6ef498c18a64abd6a72edb08384a9
bobtop42 <nv714774@gmail.com>
2025-10-03 18:16:52 -0700

    Update navsys.h

--- navsys.h
+++ navsys.h
@@ -1,66 +1,33 @@
 #include <cmath>
-
+#include "plane.h"
+#include "io.h"
 #ifndef NAVSYS_H
 #define NAVSYS_H
 
-//NOTE: add npc counter. Burrently only a hardcoded int 
-
-struct pt3D{float x = 0.0f; float y = 0.0f; float z = 0.0f;};
-
-struct angle{float pitch = 0.0f; float roll = 0.0f;};
-
-struct flapPos
-{
-  float fL = 0.0f; float fR = 0.0f;
-  flapPos& operator+=(const flapPos& other)
-  {
-    fL += other.fL;
-    fR += other.fR;
-    return *this;
-  }
-  void rad(flapPos& myFlapPos);
-};
-
-struct EP
-{
-  flapPos elevator;
-  const float MAX_POS = 60.0f; // placeholder until we get real data
-  void elevatorAdj(EP ep);
-};
-
-struct AP
-{
-  flapPos aileron;
-  const float MAX_POS = 60.0f; // placeholder until we get real data
-  void aileronAdj(AP ep);
-};
-
-struct Plane
-{
-  pt3D loc;
-  angle WPA;
-  angle PA;
-  EP ep;
-  AP ap;
-  float speed;
-};
+//NOTE: add npc counter. Burrently only a hardcoded int
 
 class NAVSYS
 {
 public:
-Plane plane;
+PLANE *navsys = &plane;
 
-int npc = 1;
+int npc = 0;
 
 const float PI = 3.14159f;
 
 void wayPointAngleFinder();
-void planeAngleFinder(float kX[3][0]); //fill in with kalman filter stuff later
+void planeAngleFinder(CKALMAN* ckalman, PLANE* plane, HMC::HMC* hmc); //fill in with kalman filter stuff later
+void updateNpc(PLANE* plane);
+
+NAVSYS();
+
+uint8_t routeCompleted;
 
 private:
+int routeLen;
 float WPXYZ[14][3];
 
-void updateEP(Plane& plane, float value);
+void updateEP(PLANE& plane, float value);
 void updateAP(float value);
 
 };
