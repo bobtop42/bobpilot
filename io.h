b23791c3f187443ed959873ff61dcf520be3ee68
bobtop42 <nv714774@gmail.com>
2025-11-07 13:04:26 -0700

    Add io.h header file with IO class definition

---
+++ io.h
@@ -0,0 +1,22 @@
+#include "plane.h"
+#include "pid.h"
+// Forward declare to avoid circular include
+class LOGGER;
+#include "kalman.h"
+#include "navsys.h"
+#ifndef IO_H
+#define IO_H
+
+PLANE plane;
+
+class IO
+{
+public:
+int setUpSim();
+int setUpReal();
+
+
+
+};
+
+#endif
