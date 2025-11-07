1de94afffbe223e1f76cfdc7722dcf5dfc77cbb5
bobtop42 <nv714774@gmail.com>
2025-11-07 12:56:49 -0700

    Add magnetic north offset calculation
    Added a function to calculate magnetic north offset based on plane's location and hemisphere. Updated measurement update to include the magnetic north offset.

--- ckalman.cpp
+++ ckalman.cpp
@@ -23,11 +23,35 @@ void CKALMAN::kGain(float (&k)[2][2])
   k[1][1] = (p[1][0]/(p[0][1] + r[0][1])) + (p[1][1]/(p[1][1] + r[1][1]));
 }
 
+double magNorthOffset(PLANE* plane)
+{
+  char NS = plane->hemisphere.NS;
+  char EW = plane->hemisphere.EW;
+
+  int temp = static_cast<int>(plane->loc.x/40.0f);
+
+  float temp2 = (static_cast<int>(!(('W'-EW)+temp)>>1));
+  float mod = 139.298f-(98.596f*(static_cast<float>(temp2)));
+
+  int addOrSub = (((static_cast<int>((!('W'-EW)<<1))+!temp)/3)<<1)-1;
+
+  float NSmod = static_cast<float>(!('S'-NS))*85.762f;
+
+  float px = plane->loc.x +(mod*static_cast<float>(addOrSub));
+  float pz = plane->loc.z + NSmod; //adust for any lat deg and hemisphere(i believe)
+
+  px = longToFeet(px, pz);
+  pz = latToFeet(pz);
+
+  return atan2(px, pz);
+}
+
 void CKALMAN::measureUpdate(float (&y)[2], PLANE* plane, HMC::HMC* hmc)
 {
   //dont forget to add the z^m matrix at some point
   y[0] = plane->PA.roll;
-  y[1] = static_cast<float>(hmc->heading(plane)); 
+  y[1] = static_cast<float>(hmc->heading(plane));
+  y[1]+=static_cast<float>(magNorthOffset(plane));
 }
 
 void CKALMAN::xUpdate(float (&x)[2][1])
@@ -62,5 +86,6 @@ void CKALMAN::loop(PLANE* plane, HMC::HMC* hmc)
   xUpdate(x);
   updateP(p);
 }
+
 
 float CKALMAN::returnX(uint pos) { return x[((pos & 0x03)^0x01)>>1][0];}
