8ac6efaabccb27c716bcc3234db221c8248e1cd4
bobtop42 <nv714774@gmail.com>
2025-11-15 13:17:32 -0700

    Update time variables in logger.cpp

--- logger.cpp
+++ logger.cpp
@@ -141,6 +141,12 @@ auto LOGGER::fillPlaneData(const std::string data, int *logPos, PLANE *plane)
   plane->npc = npc;
 
   //work on this later
-  plane->time.updateTime(UTChrs, UTCmin, UTCsec, plane);
+  plane->time.prev_hrs_ = plane->time.hrs_;
+  plane->time.prev_min_ = plane->time.min_;
+  plane->time.prev_sec_ = plane->time.sec_;
+
+  plane->time.hrs_ = UTChrs;
+  plane->time.min_ = UTCmin;
+  plane->time.sec_ = UTCsec;
   
 }
