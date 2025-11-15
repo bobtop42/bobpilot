108f6926726e09f3b3ba61d2350c923d8a80cb41
bobtop42 <nv714774@gmail.com>
2025-11-15 13:24:43 -0700

    Create replayio.h

---
+++ replayio.h
@@ -0,0 +1,61 @@
+#ifndef REPLAY_H
+#define REPLAY_H
+#include "atc.h"
+#include "gps.h"
+#include "hold.h"
+#include "i2c.h"
+#include "kalman.h"
+#include "logger.h"
+#include "M2H.h"
+#include "MATHLIB.h"
+#include "pca9685.h"
+#include "pid.h"
+#include "pitch.h"
+#include "roll.h"
+#include "screen.h"
+
+class REPLAY
+{
+private:
+std::string filename = "logTemp.txt";
+
+public:
+NAVSYS flightComputer;
+HOLD holdComputer;
+PLANE plane;
+
+PITCH pitch{0.0f, 0.0f, false};
+ROLL roll{0.0f, 0.0f, false};
+//ATC atc{0.0f, 0.0f, false};
+
+REPLAY();
+
+I2C mpu;
+GPS gps;
+PCA servos;
+M2H motors;
+
+LOGGER logger{filename};
+KALMAN kalman;
+
+void printStatus();
+void setUp();
+void update();
+void loop();
+
+void targetAll();
+
+void engageAll();
+void disengageAll();
+void engaged();
+void disengaged();
+
+bool isRunning;
+bool setUp_ = false;
+bool engaged_ = false;
+
+private:
+int setUpCounter = 0;
+};
+
+#endif
