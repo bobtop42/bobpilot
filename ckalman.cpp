b273bee9ce0e586c779a528b0d8b5d2ef979c65e
bobtop42 <nv714774@gmail.com>
2025-11-15 13:14:03 -0700

    Refactor CKALMAN methods to remove parameters

--- ckalman.cpp
+++ ckalman.cpp
@@ -1,13 +1,13 @@
-#include "ckalman.h"
+#include "MATHLIB.h"
 
-void CKALMAN::xPred(float (&x)[2][1])
+void CKALMAN::xPred()
 {
   //fill in with other matrixis later
   x[0][0] = x[0][0] + 0.1f * x[1][0];
   x[1][0] = x[1][0]; // check this
 }
 
-void CKALMAN::pPred(float (&p)[2][2])
+void CKALMAN::pPred()
 {
   p[1][0] += 0.1f * p[1][1] + q[1][0];
   p[0][0] += 0.1f * (p[0][1] + p[1][0]) + q[0][0];
@@ -15,7 +15,7 @@ void CKALMAN::pPred(float (&p)[2][2])
   p[1][1] += q[1][1];
 }
 
-void CKALMAN::kGain(float (&k)[2][2])
+void CKALMAN::kGain()
 {
   k[1][0] = (p[1][0]/(p[0][0] + r[0][0])) + (p[1][1]/(p[1][0] + r[1][0]));
   k[0][0] = (p[0][0]/(p[0][0] + r[0][0])) + (p[0][1]/(p[1][0] + r[1][0]));
@@ -46,7 +46,7 @@ double magNorthOffset(PLANE* plane)
   return atan2(px, pz);
 }
 
-void CKALMAN::measureUpdate(float (&y)[2], PLANE* plane, HMC::HMC* hmc)
+void CKALMAN::measureUpdate(PLANE* plane, HMC::HMC* hmc)
 {
   //dont forget to add the z^m matrix at some point
   y[0] = plane->PA.roll;
@@ -54,7 +54,7 @@ void CKALMAN::measureUpdate(float (&y)[2], PLANE* plane, HMC::HMC* hmc)
   y[1]+=static_cast<float>(magNorthOffset(plane));
 }
 
-void CKALMAN::xUpdate(float (&x)[2][1])
+void CKALMAN::xUpdate()
 {
   y[0] -= x[0][0];
   y[1] -= x[1][0];
@@ -63,7 +63,7 @@ void CKALMAN::xUpdate(float (&x)[2][1])
   x[1][0] += (k[1][0] * y[0] + k[1][1] * y[1]) * 2.0f;
 }
 
-void CKALMAN::updateP(float (&p)[2][2])
+void CKALMAN::updateP()
 {
   i[0][0] -= k[0][0]; k[0][0] = p[0][0];
   i[0][1] -= k[0][1]; k[0][1] = p[0][1];
@@ -79,12 +79,12 @@ void CKALMAN::updateP(float (&p)[2][2])
 void CKALMAN::loop(PLANE* plane, HMC::HMC* hmc)
 {
   //compass kalman filter loop
-  xPred(x);
-  pPred(p);
-  kGain(k);
-  measureUpdate(y, plane, hmc);
-  xUpdate(x);
-  updateP(p);
+  xPred();
+  pPred();
+  kGain();
+  measureUpdate(plane, hmc);
+  xUpdate();
+  updateP();
 }
 
 
