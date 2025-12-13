a8bc9a42c6d7d23403cfa8ccdc76b3da79899fb1
bobtop42 <nv714774@gmail.com>
2025-12-13 16:08:33 -0700

    Add preAngleCalc.h with PLANEFT structure and normalize

---
+++ preAngleCalc.h
@@ -0,0 +1,24 @@
+#ifndef PREANGLECALC_H
+#define PREANGLECALC_H
+#include "WPROUTE.h"
+#include "MATHLIB.h"
+
+struct PLANEFT
+{
+float wxft, wyft, wzft;
+float pxft, pyft, pzft;
+
+void normalize(PLANE *plane)
+{
+  wxft = plane->WPXYZ.nextWPpos->x;
+  wyft = plane->WPXYZ.nextWPpos->y;
+  wzft = plane->WPXYZ.nextWPpos->z;
+
+  pxft = longToFeet(plane->loc.x, plane->loc.z);
+  pyft = plane->loc.y;
+  pzft = latToFeet(plane->loc.z);
+}
+
+};
+
+#endif
