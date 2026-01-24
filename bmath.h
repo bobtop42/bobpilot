00aa5a94d96576b81cf2974710a6ec9e0beaec06
bobtop42 <nv714774@gmail.com>
2026-01-23 17:01:21 -0700

    Enhance bmath.h function comments for accuracy
    Updated function declarations in bmath.h to include accuracy notes.

--- bmath.h
+++ bmath.h
@@ -1,16 +1,16 @@
 #pragma once
 #include "arm_neon.h"
 
-float bsin(float x); //done
-float bcos(float x); //done
+float bsin(float x); //done accurate upto 360 deg
+float bcos(float x); //done accurate upto 360 deg
 float btan(float x);
 
 float bacos(float x);
 float basin(float x);
-float batan2(float y, float x); //done
+float batan2(float y, float x); //done accurate within ~90deg
 
-float qsqrt(float number); //done
-float bfabs(float x); //done
+float qsqrt(float number); //done ~ 1% error margin
+float bfabs(float x); //done accurate always, but no NaN, or inf checks
 float bfmod(float value, float mod);
 
 union foriunion {float f; int i;};
