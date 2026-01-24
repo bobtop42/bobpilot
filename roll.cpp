ed9a548786a7265c9ceed1346888729494935331
bobtop42 <nv714774@gmail.com>
2026-01-23 17:06:58 -0700

    Refactor targetRoll calculations to use bfabs

--- roll.cpp
+++ roll.cpp
@@ -14,8 +14,8 @@ void ROLL::disengaged()
 
 void ROLL::targetRoll(float value, PLANE* plane)
 {
-    targetRoll_ = fabs(value);
-    RollDir = static_cast<int8_t>(value/fabs(value) + static_cast<float>(!static_cast<int>(fabs(value) + 0.9999999f)));
+    targetRoll_ = bfabs(value);
+    RollDir = static_cast<int8_t>(value/bfabs(value) + static_cast<float>(!static_cast<int>(bfabs(value) + 0.9999999f)));
     /*
     returns 1 for rolling dir left, 0 for on course for roll, and -1 for rolling dir right.
     code basicaly  does this:
@@ -29,7 +29,15 @@ void ROLL::targetRoll(PLANE* plane)
     float pa = plane->PA.roll; 
     float wp = plane->WPA.roll;
 
-    targetRoll_ = fabs(static_cast<float>(static_cast<int>(fabs(wp - pa)/6.28318f + 0.5f) * 6.28318f)) - fabs(wp - pa);//check last fabs if its in the first fabs parathesis
+    targetRoll_ = bfabs(wp-pa);
+
+    float t1 = static_cast<float>(!!static_cast<int>(targetRoll_ / 3.14159f));
+    targetRoll_ = bfabs(targetRoll_ - (6.28318f * t1));
+    
+    union pon{int i; float f;};
+    pon rollpon; rollpon.f = wp - pa;
+    int t3 = static_cast<int>(t1 + !(rollpon.i&0x8000) - 2) & 0x2;
+    RollDir = static_cast<int8_t>(t3) - 1*!!rollpon.i;
 }
 
 void ROLL::adjustAileron(float value, PLANE* plane)
