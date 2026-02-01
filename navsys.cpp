abad814a714ca45770e0344501557b4a2583c7e4
bobtop42 <nv714774@gmail.com>
2026-01-31 17:12:31 -0700

    Document NAVSYS class changes and error calculation
    Added notes on NAVSYS class refactoring and temporary retention of old code for visibility.

--- navsys.cpp
+++ navsys.cpp
@@ -71,12 +71,74 @@ void NAVSYS::updateNpc(PLANE* plane)
   routeCompleted = static_cast<uint8_t>(!wpaddr);
 }
 
-/*
-void NAVSYS::setUpRoute()
-{
-  std::cout<<"enter a number of waypoints for the route: \n";
-  std::cin>>routeLen;
 
-  float WPROUTEPOINTERS[this->routeLen][3] = new float;
-}
+/*
+NOTE: The NAVSYS class is currently being redone in order to be more efficient. When completed, NAVSYS will no longer use trig. functions but rater a more linearized approach to calculating error. This means that the new system will not be usng radians or degrees to quatify errors. If anyone reading this has any suggestions please let me know.
+
+Moreover, other parts of the the AP are also being modified to work with the new system. Basically the ROLL, PITCH, and ATC classes are being "re-plumbed". Additionally the HOLD class will also see significant changes as well.
+
+I will temptorary be leaving the old code in place for now, so progression can be watched, or whatever the people who view this code do, can see the changes too, and not be shocked with one big update.
 */
+void NAVSYS::errorCalc(PLANE *plane)
+{
+px = plane->pAngle[0][0];
+py = plane->pAngle[0][1];
+pz = plane->pAngle[0][2];
+
+float rplaneSlope = px/pz;
+
+foriunion x; foriunion y; foriunion z;
+
+x.f = longToFeet( plane->loc.z, plane->loc.x) - plane->planeft.wxft;
+y.f = plane->loc.y - plane->planeft.wyft;
+z.f = latToFeet( plane->loc.z) - plane->planeft.wzft;
+
+float rradius = qsqrt(x.f * x.f + z.f * z.f);
+float rplaneSlope2 = rplaneSlope * rplaneSlope;
+float rradius2 = rradius * rradius;
+
+float rx2z2 = qsqrt( px * px + pz * pz);
+float pplaneSlope = py / rx2z2;
+float pplaneSlope2 = pplaneSlope * pplaneSlope;
+float pradius = qsqrt( rradius2 + y.f * y.f);
+float pradius2 = pradius * pradius;
+
+float32x2_t vr = {rradius2, pradius2};
+float32x2_t vs = {rplaneSlope2, pplaneSlope2};
+float32x2_t vh = vdup_n_f32(0.5f);; /*fill in with the vector fill cmd*/
+
+vs = vrecipq_f32(vs);
+vr = vmulq_f32(vr, vs);
+vr = vmulq_f32(vr, vh);
+
+rradius2 = vgetq_lane_f32(vr, 0);
+pradius2 = vgetq_lane_f32(vr, 1);
+
+foriunion ra; foriunion pa;
+ra.f = qsqrt(rradius2); pa.f = qsqrt(pradius2);
+
+px = ra.f; pz = ra.f * rplaneSlope;
+py = pa.f * pplaneSlope; /*check the mul*/
+
+float rwpSlope = z.f/x.f; float pwpSlope = y.f/rradius;
+
+ra.f *= rwpSlope; pa.f *= pwpSlope;
+ra.f -= px; ra.i &= 0x80000000;
+pa.f -= rx2z2; pa.i &= 0x80000000;
+
+px -= plane->planeft.wxft;
+py -= plane->planeft.wyft;
+pz -= plane->planeft.wzft;
+
+foriunion rerror; foriunion perror;
+rerror.f = qsqrt(px * px + pz * pz) / (2.0f *rradius);
+perror.f = qsqrt(py * py + rradius * rradius) / (2.0f *pradius);
+
+rerror.i |= ra.i ^ (x.i & 0x80000000);
+x.f = rradius; //check this line
+perror.i |= pa.i ^ (x.i & 0x80000000); //check this line too
+
+plane->error.roll = rerror.f;
+plane->error.pitch = perror.f;
+/*look into weighing the errors*/
+}
