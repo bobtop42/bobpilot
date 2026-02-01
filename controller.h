cd5e432879f8522e5313ae1f13e8dc201f08b7c4
bobtop42 <nv714774@gmail.com>
2026-01-31 17:48:33 -0700

    Refactor CONTROLLER class and add UART setup
    Refactor variable names and add new methods for UART setup and toggling.

--- controller.h
+++ controller.h
@@ -5,8 +5,8 @@
 #include "sys/ioctl.h"
 #include "sys/types.h"
 #include "stdint.h"
-#include <fcntl.h>
-#include <cstdint>
+#include <fcntl.h>                  
+#include <cstdint>        
 
 class CONTROLLER
 {
@@ -14,10 +14,12 @@ public:
 void update(PLANE* plane);
 void inputChannelData(PLANE* plane, uint8_t* buf, int start);
 
+int setUpUART();
+
 void engageAP();
 void disengageAP();
 
-void integrateControlls(PLANE* plane);
+void integrateControls(PLANE* plane);
 
 CONTROLLER();
 
@@ -25,31 +27,33 @@ uint8_t flags = 0x2;
 /*
 0x01 correct crc
 0x02 AP control, SC = -100
+
 0x04 set hold pos, SC = 100
 0x08 SA bit on/off
 0x10 adj flap
 0x20 landing/takeoff bit
-0x40 SE(push botton) on/off
+0x20 se(the push button) on/off
+ox40 landing/takeoff bit
 */
+
+int8_t toggleables = 0x00;
 
 void integrateAileron(FLAP *ap);
 void integrateElevator(FLAP *ep);
 void integrateThrottle(ATC * atc);
 void integrateFlap(FLAP *fp);
-void integrateRudder(FLAP *rp);
 
-void updateControllMode();
-void updateLandingMode();
-
-int16_t airleron = 0;
+int16_t aileron = 0;
 int16_t elevator = 0;
 int16_t throttle = 0;
-int16_t rudder = 0;
 
 int8_t toggleable = 0x00;
-int8_t sa;
+int8_t sa = 0x00;
 
-uint16_t ail, ele, thr, rud, SA,SB, SC, SD, SE, SF;
+
+void toggleSA();
+
+int16_t ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8, ch9, ch10;
 
 int fd;
 };
