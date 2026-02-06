8dbdb84a5033a280cdad54a28b13ccc6322c4d86
bobtop42 <nv714774@gmail.com>
2026-02-06 12:34:51 -0800

    Add new mathematical functions to bmath.h

--- bmath.h
+++ bmath.h
@@ -9,8 +9,21 @@ float bacos(float x);
 float basin(float x);
 float batan2(float y, float x); //done accurate within ~90deg
 
+float inverse_qsqrt(float number);
 float qsqrt(float number); //done ~ 1% error margin
+
 float bfabs(float x); //done accurate always, but no NaN, or inf checks
 float bfmod(float value, float mod);
+
+float bln(float x);
+inline float btrunc(float x){return static_cast<float>(static_cast<int32_t>(x));}
+
+float bpow_no_decimal(float x, float y);
+float pow(float x, float y);
+
+float bexp(float x);
+
+float sigmoid(float x);
+float relu(float x);
 
 union foriunion {float f; int i;};
