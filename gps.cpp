01ac4d6c5996c9ef14e60321cf1b95de2345e693
bobtop42 <nv714774@gmail.com>
2026-02-07 14:45:24 -0800

    Refactor GPS class methods for better structure

--- gps.cpp
+++ gps.cpp
@@ -1,241 +1,203 @@
 #include "gps.h"
+#include <tuple>
 
-void GPS::openSerialFD()
+void GPS::punctuationMarker(const std::string gpsmsg)
 {
-    fd = open("/dev/ttyAMA5", O_RDWR | O_NOCTTY | O_SYNC);
-    if (fd < 0)
+  int i = 0;
+  int pc = 1; //punctuation counter
+  int gpsmsglen = gpsmsg.length();
+  for(int i2=0; i<gpsmsglen; i2++)
+  {
+    if(gpsmsg[i2]=='$'){puncLocation[0]=i2; i = i2; break;}
+  }
+
+  i++;
+  
+  while(gpsmsg[i]!='\r')
     {
-        std::cerr << "ERROR: failed to open fd\n";
-        SHUTDOWNERROR = true;
+      puncLocation[pc] = i;
+      pc = pc + !(gpsmsg[i]-',') + !(gpsmsg[i]-'*') + !(gpsmsg[i]-'.') + !(gpsmsg[i]-'-');
+      i++;
     }
 }
 
-bool GPS::configureSerialPort()
+/*std::tuple<int, int, int, float, const char, float, const char, int, float> */ auto GPS::parser(const std::string gpsmsg, int* puncLocation)//-> std::tuple<int,int,int,float,char,float,char,int,float>
 {
-    speed_t baudRate = 9600;
-    termios tty{};
+  if(gpsmsg.substr(puncLocation[0]+1,6) != "GPGGA")
+  {
+    GPGGA = true;
+  }
 
-    if (tcgetattr(fd, &tty) != 0)
-    {
-        perror("error 1 from tcgetattr");
-        SHUTDOWNERROR = true;
-        close(fd);
-        return false;
-    }
+  int i=1;
+  //UTC itme
+  //hours
+  int hrs = std::stoi(gpsmsg.substr(puncLocation[i]+1, 2));
+  //minutes
+  int min = std::stoi(gpsmsg.substr(puncLocation[i]+3, 2));
+  //seconds
+  int sec = std::stoi(gpsmsg.substr(puncLocation[i]+5, 2));
 
-    cfsetispeed(&tty, baudRate);
+  i+=2;
+  int t = (!(gpsmsg[i+1]-'-'));
+  //lat in deg format DDMM.MMMM
+  float lat = std::stof(gpsmsg.substr(puncLocation[i]+1, t+2));
+  i+=1+t;
 
-    tty.c_cflag &= ~PARENB;
-    tty.c_cflag &= ~CSTOPB;
-    tty.c_cflag &= ~CSIZE;
-    tty.c_cflag |= CS8;
+  float latMin = std::stof(gpsmsg.substr(puncLocation[i]-2,1+puncLocation[i+1]-puncLocation[i]));
+  float gpsLatLocation = lat + latMin/60.0f;
+  i++;
+  char latHemisphere = gpsmsg[puncLocation[i]+1];
+  t=(!gpsmsg[i+1]-'-');
+  //long in deg in DDDMM.MMMM format
+  float Long = std::stoi(gpsmsg.substr(puncLocation[i]+1,3+t));
+  i+=1+t;
+  float LongMin = std::stof(gpsmsg.substr(puncLocation[i]-2, 1+puncLocation[i+1]-puncLocation[i]));
+  float gpsLongLocation = Long + LongMin/60.0f;
+  i++;
+  char LongHemisphere = gpsmsg[puncLocation[i]+1];
+  i++;
+  int gpsMeasureAccuracy = std::stoi(gpsmsg.substr(puncLocation[i]+1, 1));
+  i++;
 
-    tty.c_cflag &= ~CRTSCTS;
-    tty.c_cflag |= CREAD | CLOCAL;
+  int numSat = std::stoi(gpsmsg.substr(puncLocation[1+i]-puncLocation[i]));
+  i++;
+  int hdop = std::stoi(gpsmsg.substr(puncLocation[i], puncLocation[i+1]-puncLocation[i]));
+  i++;
+  float gpsAlt = std::stof(gpsmsg.substr(puncLocation[i], 1+puncLocation[i+1]-puncLocation[i]));
+  i++;
+  char gpsAltUnit = gpsmsg[puncLocation[i]+1];
+  i++;
+  
+  if(toupper(gpsAltUnit) == 'M')
+  {
+    gpsAlt = toFeet(gpsAlt);
+  }
 
-    tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
-    tty.c_iflag &= ~(ICRNL | INLCR);
-    tty.c_oflag &= ~OPOST;
+  float gpsGeoidalSep = std::stof(gpsmsg.substr(puncLocation[i], 1+puncLocation[i+1]-puncLocation[i]));
 
-    if (tcsetattr(fd, TCSANOW, &tty) != 0)
-    {
-        perror("error 2 from tcsetattr");
-        SHUTDOWNERROR = true;
-        close(fd);
-        return false;
-    }
-    return true;
+ char gpsGeoidalSepUnit = gpsmsg[puncLocation[i]+1];
+  
+  if(toupper(gpsGeoidalSepUnit)== 'M')
+  {
+    gpsAlt -= toFeet(gpsGeoidalSep);
+  }
+  else
+  {
+    gpsAlt -= gpsGeoidalSep;
+  }
+
+  return std::make_tuple(hrs, min, sec, gpsLatLocation, latHemisphere, gpsLongLocation, LongHemisphere, gpsMeasureAccuracy, gpsAlt, numSat,hdop);
 }
 
-void GPS::markPunctuationPositions(const std::string gpsmsg)
+void GPS::update(PLANE* plane, int tries)
 {
-    int i = 0;
-    int pc = 1;
-    int gpsmsglen = gpsmsg.length();
+  int fd = open ("/dec/ttyS0", O_RDWR|O_NOCTTY|O_SYNC);
 
-    for (int i2 = 0; i < gpsmsglen; i2++)
-    {
-        if (gpsmsg[i2] == '$')
-        {
-            puncLocation[0] = i2;
-            i = i2;
-            break;
-        }
-    }
-
-    i++;
-
-    while (gpsmsg[i] != '\r')
-    {
-        puncLocation[pc] = i;
-        pc = pc + !(gpsmsg[i] - ',') + !(gpsmsg[i] - '*') + !(gpsmsg[i] - '.') + !(gpsmsg[i] - '-');
-        i++;
-    }
-}
-
-auto GPS::parseGPGGA(const std::string gpsmsg, int puncLocation[67])
-{
-    int hours;
-    int minutes;
-    int seconds;
-
-    float gpsLat;
-    char latHemisphere;
-    float gpsLong;
-    char longHemisphere;
-
-    int measureAccuracy;
-    int numSatellites;
-
-    int hdop;
-    float altitude;
-    float geoidalSep;
-
-    if (gpsmsg.substr(puncLocation[0] + 1, 6) != "GPGGA")
-    {
-        goodRead = false;
-        return std::make_tuple(hours, minutes, seconds, gpsLat, latHemisphere,
-                               gpsLong, longHemisphere, measureAccuracy,
-                               altitude, numSatellites, hdop, false);
-    }
-    else
-    {
-        goodRead = true;
-    }
-
-    int j = 0;
-    while (gpsmsg[j] != '\0')
-    {
-        std::cout << gpsmsg[j];
-        j++;
-    }
-    std::cout << "\n";
-
-    int i = 1;
-    hours = std::stoi(gpsmsg.substr(puncLocation[i] + 1, 2));
-    minutes = std::stoi(gpsmsg.substr(puncLocation[i] + 3, 2));
-    seconds = std::stoi(gpsmsg.substr(puncLocation[i] + 5, 2));
-
-    i += 2;
-    int t = (!(gpsmsg[i + 1] - '-'));
-    float latDegrees = std::stof(gpsmsg.substr(puncLocation[i] + 1, t + 2));
-    i += 1 + t;
-
-    float latMinutes = std::stof(gpsmsg.substr(puncLocation[i] - 2, 1 + puncLocation[i + 1] - puncLocation[i]));
-    gpsLat = latDegrees + latMinutes / 60.0f;
-    i++;
-    latHemisphere = gpsmsg[puncLocation[i] + 1];
-
-    t = (!gpsmsg[i + 1] - '-');
-    float longDegrees = std::stoi(gpsmsg.substr(puncLocation[i] + 1, 3 + t));
-    i += 1 + t;
-
-    float longMinutes = std::stof(gpsmsg.substr(puncLocation[i] - 2, 1 + puncLocation[i + 1] - puncLocation[i]));
-    gpsLong = longDegrees + longMinutes / 60.0f;
-    i++;
-    longHemisphere = gpsmsg[puncLocation[i] + 1];
-    i++;
-    measureAccuracy = std::stoi(gpsmsg.substr(puncLocation[i] + 1, 1));
-    i++;
-    numSatellites = std::stoi(gpsmsg.substr(puncLocation[1 + i] - puncLocation[i]));
-    i++;
-    hdop = std::stoi(gpsmsg.substr(puncLocation[i], puncLocation[i + 1] - puncLocation[i]));
-    i++;
-    altitude = std::stof(gpsmsg.substr(puncLocation[i], 1 + puncLocation[i + 1] - puncLocation[i]));
-    i++;
-    char altitudeUnit = gpsmsg[puncLocation[i] + 1];
-    i++;
-
-    if (toupper(altitudeUnit) == 'M')
-    {
-        altitude = toFeet(altitude);
-    }
-
-    geoidalSep = std::stof(gpsmsg.substr(puncLocation[i], 1 + puncLocation[i + 1] - puncLocation[i]));
-    char geoidalUnit = gpsmsg[puncLocation[i] + 1];
-
-    if (toupper(geoidalUnit) == 'M')
-    {
-        altitude -= toFeet(geoidalSep);
-    }
-    else
-    {
-        altitude -= geoidalSep;
-    }
-
-    return std::make_tuple(hours, minutes, seconds, gpsLat, latHemisphere,
-                           gpsLong, longHemisphere, measureAccuracy,
-                           altitude, numSatellites, hdop, true);
-}
-
-void GPS::processGPSMessage(PLANE* plane, const std::string gpsmsg)
-{
-    this->markPunctuationPositions(gpsmsg);
-
-    auto [hrs, min, sec, lat, hem1, Long, hem2, Acc, Alt, numSat, hdop, parseGPGGAdataFlag] =
-        parseGPGGA(gpsmsg, puncLocation);
-
-    if (parseGPGGAdataFlag)
-    {
-        plane->updateGPS(lat, Alt, Long, hem1, hem2, Acc, hrs, min, sec, numSat, hdop);
-        goodRead = true;
-    }
-    else
-    {
-        goodRead = false;
-    }
-}
-
-void GPS::readAndParseGPS(PLANE* plane, int tries)
-{
+  if(fd==-1)
+  {
+    GPGGA = false;
+  }
+  else
+  {
     char buf[256];
-    std::string sentence = "";
-
+    std::string sentence="";
+  
     int n = read(fd, buf, sizeof(buf));
 
-    if (n > 0)
+    if(n>0)
     {
-        for (int i = 0; i < n; ++i)
+      for(int i=0; i<n; ++i)
         {
-            char ch = buf[i];
-            if (ch == '\n')
-            {
-                break;
-            }
-            else if (ch != '\n')
-            {
-                sentence += ch;
-            }
+          char ch = buf[i];
+          if(ch == '\n')
+          {
+            break;
+          }
+          else if(ch!='\n')
+          {
+            sentence += ch;
+          }
         }
     }
     else
     {
-        GPGGA = false;
+      close(fd);
+      GPGGA = false;
     }
-
-    if (!sentence.empty() && sentence.find("$GPGGA") != std::string::npos)
-    {
-        processGPSMessage(plane, sentence);
-    }
-
+    update(plane, sentence);
     sentence.clear();
-    GPGGA = goodRead;
+    GPGGA = true;
+  }
+  close(fd);
 }
 
-void GPS::refreshGPSData(PLANE* plane)
+void GPS::update(PLANE* plane)
 {
-    int tries = 0;
-
-    while (tries < 3)
+  int tries = 0;
+  while(tries<3)
     {
-        readAndParseGPS(plane, tries);
-        if (goodRead)
-            break;
-        else
-            tries++;
+      update(plane, tries);
+      if(GPGGA)
+        break;
+      else
+        tries++;
     }
+  if(!GPGGA)
+    SHUTDOWNERROR = true;
+}
 
-    if (!GPGGA)
-        SHUTDOWNERROR = true;
+void GPS::update(PLANE* plane, const std::string gpsmsg)
+{
+  this->punctuationMarker(gpsmsg);
+
+  auto[hrs, min, sec, lat, hem1, Long, hem2, Acc, Alt, numSat, hdop] = parser(gpsmsg, puncLocation);
+
+  plane->updateGPS(lat, Alt, Long, hem1, hem2, Acc, hrs, min, sec, numSat, hdop);
+}
+
+bool GPS::configSerialPort(int fd)
+{
+  speed_t baudRate=9600;
+  
+  termios tty{};
+  //check tcgetaddr 
+  if(tcgetattr(fd, &tty)!=0)
+  {
+    perror("error 1 from tcgetattr");
+    close(fd);
+    return false;
+  }
+  
+  //set baud rate
+  cfsetispeed(&tty, baudRate);
+
+  //config bit rate etc
+  tty.c_cflag &= ~PARENB; //no parity
+  tty.c_cflag &= ~CSTOPB; //stop bit
+  tty.c_cflag &= ~CSIZE; //check here and clear all bits
+  tty.c_cflag |= CS8; //2 nibbles/cycle
+
+  //flow ctrl
+  tty.c_cflag &= ~CRTSCTS; //check here and no flow ctrl 
+
+  //engage read & ignore
+  tty.c_cflag |= CREAD | CLOCAL;
+
+  //set raw data
+  //no cname, echo etc
+  tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+
+  //more ctrl settings
+  tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+  tty.c_iflag &= ~(ICRNL | INLCR);
+  tty.c_oflag &= ~OPOST;
+
+  if(tcsetattr(fd, TCSANOW, &tty)!=0)
+  {
+    perror("error 2 from tcsetattr");
+    close(fd);
+    return false;
+  }
+  close(fd);
+  return true;
 }
