95a6fdd589ae953fa38454300312a60de7524ad9
bobtop42 <nv714774@gmail.com>
2026-02-07 14:45:51 -0800

    Refactor GPS class header and update method signatures

--- gps.h
+++ gps.h
@@ -1,39 +1,32 @@
-#pragma once
-
+#ifndef GPS_H
+#define GPS_H
 #include "MATHLIB.h"
 #include <string>
 #include <iostream>
 #include <fstream>
 #include <tuple>
 #include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <linux/i2c-dev.h>
-#include <chrono>
-#include <thread>
+#include <tuple>
+
 
 class GPS
 {
 public:
-    bool goodRead = true;
+void update(PLANE* plane, const std::string gpsmsg);
+void update(PLANE* plane);
+bool configSerialPort(int fd);
 
-    void openSerialFD();
-    bool configureSerialPort();
+GPS();
 
-    void markPunctuationPositions(const std::string gpsmsg);
-    auto parseGPGGA(const std::string gpsmsg, int puncLocation[67]);
+private:
+void update(PLANE* plane, int tries);
+void punctuationMarker(const std::string gpsmsg);
+auto parser(const std::string gpsmsg, int puncLocation[67]);
 
-    void processGPSMessage(PLANE* plane, const std::string gpsmsg);
-    void readAndParseGPS(PLANE* plane, int tries);
-    void refreshGPSData(PLANE* plane);
-
-    float gpsAltitude;
-    int fd;
-
-    int puncLocation[67];
-    bool gpsERRORepen = false;
-    bool gpsERRORread = false;
-    bool GPGGA = true;
+int puncLocation[67];
+bool gpsERRORepen = false;
+bool gpsERRORread = false;
+bool GPGGA = true;
 };
+
+#endif
