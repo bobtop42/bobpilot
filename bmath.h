8f9f6341518fab15839debe6a71588ffe2c6b4bb
bobtop42 <nv714774@gmail.com>
2026-02-07 16:04:29 -0800

    Add new mathematical functions and update existing ones

--- bmath.h
+++ bmath.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "arm_neon.h"
+#include "stdint.h"
+#include <cstring>
+#define _e_ 2.7182818284590452353602874713527f
 
 float bsin(float x); //done accurate upto 360 deg
 float bcos(float x); //done accurate upto 360 deg
@@ -9,21 +12,26 @@ float bacos(float x);
 float basin(float x);
 float batan2(float y, float x); //done accurate within ~90deg
 
-float inverse_qsqrt(float number);
+float qinverse_qsqrt(float number); // done ~ 1% error margin
 float qsqrt(float number); //done ~ 1% error margin
 
 float bfabs(float x); //done accurate always, but no NaN, or inf checks
 float bfmod(float value, float mod);
 
-float bln(float x);
-inline float btrunc(float x){return static_cast<float>(static_cast<int32_t>(x));}
+float bln(float x); //done
+inline float btrunc(float x){return static_cast<float>(static_cast<int32_t>(x));} //done (duh, inline)
 
-float bpow_no_decimal(float x, float y);
-float pow(float x, float y);
+float bpow_no_decimal(float x, float y);//done
+float bpow(float x, float y); //done
 
-float bexp(float x);
+float bexp_decimal(float x); //done
+float bexp_no_decimal(float x); //done
+float bexp(float x); //done
 
-float sigmoid(float x);
-float relu(float x);
+float sigmoid(float x); //done
+float relu(float x); //done
+
+float sigmoid_derivative(float x); // done
+float relu_derivative(float x); //done
 
 union foriunion {float f; int i;};
