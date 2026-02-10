97a22734a1dd457f66868385106c6d431831ca40
bobtop42 <nv714774@gmail.com>
2026-02-10 16:23:16 -0700

    Refactor NAVSYS methods and improve calculations

--- navsys.cpp
+++ navsys.cpp
@@ -1,210 +1,136 @@
-#include "kalman.h"
-#include "io.h"
+#include "navsys.h"
+//#include "kalman.h"
 
-void KALMAN::xPred(long int dt)
+//READ ME: num of wp vals set in main? 14 for rn tho
+
+NAVSYS::NAVSYS():npc(0), routeLen(14) {};
+
+//wayPointAngleFinder has been updated! Nww completely branchless. yay. 
+void NAVSYS::wayPointAngleFinder(PLANE* plane)
 {
-  float AX[6];
-  float BU [6];
+  float wx = plane->planeft.wxft;
+  float wy = plane->planeft.wyft;;
+  float wz = plane->planeft.wzft;
 
-  for(int i=0; i<3; ++i)
-    {
-      for(int j=0; j<3; ++j)
-        {
-          AX[i] = (A[i][j] * X[i][0])  + (A[i][j] * X[i+3][0]);
-          AX[i+3] = (A[i][j] * X[i+3][0]);
+  float px = plane->planeft.pxft;
+  float py = plane->planeft.pyft;
+  float pz = plane->planeft.pzft;
 
-          BU[i] += B[i][j] * AC[i][0];
-          BU[i+3] += B[i+3][j] * AC[i][0];
-        }
+  wy-=py; wx-=px; wz-=pz;
 
-      X[i][0] = AX[i] + BU[i];
-      X[i+3][0] = AX[i+3] + BU[i+3];
-    }
-}
+  plane->WPA.roll = bfabs(batan2(wx,wz)) + ((wx/bfabs(wx) - 1.0f) * -1.570795f) + ((wz/bfabs(wz) - 1.0f) * -0.7853795);
+  plane->WPA.pitch = batan2(wy, (qsqrt(wx * wx + wz * wz)));
+};
 
-void KALMAN::pPred(long int dt)
+//fill in wiht kalman filter stuff later: maybevoid NAVSYS::planeAngleFinder(KALMAN* kalman)???
+//also planePointAngleFinder has been updated! Nww completely branchless. yay.
+void NAVSYS::planeAngleFinder(CKALMAN* ckalman, PLANE* plane, HMC::HMC* hmc)
 {
-  float loadv[4];
-  float loadv2[2];
-  float dtf = static_cast<float>(dt);
-  float32x4_t v1 = {dtf, dtf, dtf, dtf}; //v1 = dt
-  float32x4_t v2 = {P[3][3], P[3][4], P[3][5], P[4][3]}; // v2 = p10
-  float32x4_t v3 = vmulq_f32(v1, v2); // v3 = v2 * v1
-  loadv[0] = P[0][3]; loadv[1] = P[0][4]; loadv[2] = P[0][5]; loadv[3] = P[1][3];
-  v2 = vld1q_f32(loadv); // v2 = p4
-  v2 = vaddq_f32(v2, v3); // v2 = v2 + v3
+  float x = plane->pAngle[0][0];
+  float y = plane->pAngle[1][0];
+  float z = plane->pAngle[2][0];
+
+  plane->PA.roll = bfabs(batan2(x, z) + ((x/bfabs(x) - 1.0f) * -1.570795f) + ((z/bfabs(z) - 1.0f) * -0.7853795));
+  plane->PA.pitch = batan2(y, (qsqrt(x * x + z * z)));
+
+  ckalman->loop(plane, hmc);
+  plane->PA.roll = ckalman->returnX(0);
   
-  P[0][3] = vgetq_lane_f32(v2, 0); //p4 = v2
-  P[0][4] = vgetq_lane_f32(v2, 1);
-  P[0][5] = vgetq_lane_f32(v2, 2);
-  P[1][3] = vgetq_lane_f32(v2, 3);
+}
+
+void NAVSYS::updateEP(PLANE& plane, float value)
+{
+  plane.ep.flap.fR = plane.ep.flap.fL += value;
+  plane.ep.flapAdj(plane.ep);
+}
+
+void NAVSYS::updateAP(PLANE& plane, float value)
+{
+  plane.ap.flap.fR = plane.ap.flap.fL += value;
+  plane.ap.flapAdj(plane.ap);
+}
+
+void NAVSYS::updateNpc(PLANE* plane)
+{
+  px = plane->planeft.pxft; py = plane->planeft.pyft; pz = plane->planeft.pzft;
+  wx = plane->planeft.wxft; wy = plane->planeft.wyft; wz = plane->planeft.wzft;
+
+  wx = bfabs(wx - px); wy = bfabs(wy - py); wz = bfabs(wz - pz);
+
+  /*plane must be withing ft of the waypoint to be counted as marked. code below dis, then truncates the flip val with !*/
+
+  uintptr_t wpIncra = (!static_cast<uintptr_t>(wx/6.0f)) *  
+                      (!static_cast<uintptr_t>(wy/6.0f)) *
+                      (!static_cast<uintptr_t>(wz/6.0f));
+
+  uintptr_t wpaddr = (reinterpret_cast<uintptr_t>(plane->WPXYZ.nextWPpos->next_)* wpIncra) + (reinterpret_cast<uintptr_t>(plane->WPXYZ.nextWPpos)* (!wpIncra));
+
+  plane->WPXYZ.nextWPpos = reinterpret_cast<WPROUTE<int>::WP*>(wpaddr);
   
-  v2 = vmulq_f32(v2, v1);// v2 = v2 * v1
-  loadv[0] = P[3][0]; loadv[1] = P[3][1]; loadv[2] = P[3][2]; loadv[3] = P[4][0];
-  float32x4_t v4 = vld1q_f32(loadv); // v4 = p7
-  v3 = vaddq_f32(v3, v4); //v3 = v3 + v4
-
-  P[3][0] = vgetq_lane_f32(v2, 0); //p7 = v2
-  P[3][1] = vgetq_lane_f32(v2, 1);
-  P[3][2] = vgetq_lane_f32(v2, 2);
-  P[4][0] = vgetq_lane_f32(v2, 3);
-
-  v4 = vmulq_f32(v4, v1); // v4 = v4 * v1
-
-  loadv[0] = P[0][0]; loadv[1] = P[0][1]; loadv[2] = P[0][2]; loadv[3] = P[1][0];
-  v3 = vld1q_f32(loadv); // v3 = p1
-  v3 = vaddq_f32(v3, v4); // v3 = v3 + v4
-  v3 = vaddq_f32(v3, v2); // v3 = v3 + v2
-
-  P[0][0] = vgetq_lane_f32(v3, 0); //p1 = v3
-  P[0][1] = vgetq_lane_f32(v3, 1);
-  P[0][2] = vgetq_lane_f32(v3, 2);
-  P[1][0] = vgetq_lane_f32(v3, 3);
-
-  loadv[0] = P[4][5]; loadv[1] = P[5][3]; loadv[2] = P[5][4]; loadv[3] = P[5][5];
-  v2 = vld1q_f32(loadv); // v2 = p11
-  v3 = vmulq_f32(v1, v2); // v3 = v2 * v1
-  loadv[0] = P[1][5]; loadv[1] = P[2][3]; loadv[2] = P[2][4]; loadv[3] = P[2][5];
-  v2 = vld1q_f32(loadv); // v2 = p5
-  v2 = vaddq_f32(v2, v3); // v2 = v2 + v3
-
-  P[1][5] = vgetq_lane_f32(v2, 0); //p5 = v2
-  P[2][3] = vgetq_lane_f32(v2, 1);
-  P[2][4] = vgetq_lane_f32(v2, 2);
-  P[2][5] = vgetq_lane_f32(v2, 3);
-
-  v2 = vmulq_f32(v2, v1); // v2 = v2 * v1
-  loadv[0] = P[4][2]; loadv[1] = P[5][0]; loadv[2] = P[5][1]; loadv[3] = P[5][2];
-  v4 = vld1q_f32(loadv); // v4 = p8
-  v3 = vaddq_f32(v3, v4); // v3 = v3 + v4
-
-  P[1][5] = vgetq_lane_f32(v2, 0); //p8 = v2
-  P[2][3] = vgetq_lane_f32(v2, 1);
-  P[2][4] = vgetq_lane_f32(v2, 2);
-  P[2][5] = vgetq_lane_f32(v2, 3);
-
-  v4 = vmulq_f32(v4, v1); // v4 = v4 * v1
-  
-  v3 = {*(P+8), *(P+12), *(P+13), *(P+14)}; // v3 = p2
-  v3 = vaddq_f32(v3, v4); // v3 = v3 + v4
-  v3 = vaddq_f32(v3, v2); // v3 = v3 + v2
-
-  P[1][2] = vgetq_lane_f32(v3, 0); //p2 = v3
-  P[2][0] = vgetq_lane_f32(v3, 1);
-  P[2][1] = vgetq_lane_f32(v3, 2);
-  P[2][2] = vgetq_lane_f32(v3, 3); 
-
-  v2 = {*(P+28)}; // v2 = p12
-  v3 = vmulq_f32(v1, v2); // v3 = v2 * v1
-  v2 = {P[1][4]}; // v2 = p6
-  v2 = vaddq_f32(v2, v3); // v2 = v2 + v3
-
-  P[1][4] = vgetq_lane_f32(v2, 0); //p6 = v2
-
-  v2 = vmulq_f32(v2, v1); // v2 = v2 * v1
-  v4 = {(P+25)}; // v4 = p9
-  v3 = vaddq_f32(v3, v4); // v3 = v3 + v4
-
-  P[1][5] = vgetq_lane_f32(v2, 0); //p9 = v2
-  P[2][3] = vgetq_lane_f32(v2, 1); 
-  P[2][4] = vgetq_lane_f32(v2, 2);
-  P[2][5] = vgetq_lane_f32(v2, 3);
-
-  v4 = vmulq_f32(v4, v1); // v4 = v4 * v1
-
-  v3 = {*(P+8), *(P+12), *(P+13), *(P+14)}; // v3 = p3
-  v3 = vaddq_f32(v3, v4); // v3 = v3 + v4
-  v3 = vaddq_f32(v3, v2); // v3 = v3 + v2
-
-  P[1][2] = vgetq_lane_f32(v3, 0); //p3 = v3
-  P[2][0] = vgetq_lane_f32(v3, 1);
-  P[2][1] = vgetq_lane_f32(v3, 2);
-  P[2][2] = vgetq_lane_f32(v3, 3);
+  //if true npc==routelen==0, then flips with ! 2x(zero -> 1, non-zero -> 0)
+  routeCompleted = static_cast<uint8_t>(!wpaddr);
 }
 
-void KALMAN::kGain()
+void NAVSYS::errorCalc(PLANE *plane)
 {
-  float temp[6][6];
+px = plane->pAngle[0][0];
+py = plane->pAngle[0][1];
+pz = plane->pAngle[0][2];
 
-  for(int i=0; i<3; ++i)
-    {
-      for(int j=0; j<3; ++j)
-        {
-          temp[i][j] = P[i][j] + R[i][j];
-          temp[i][j+3] = P[i][j+3] + R[i][j+3];
-          temp[i+3][j] = P[i+3][j] + R[i+3][j];
-          temp[i+3][j+3] = P[i+3][j+3] + R[i+3][j+3];
+float rplaneSlope = px/pz;
 
-          K[i][j] = P[i][j]/temp[j][i];
-          K[i][j+3] = P[i][j+3]/temp[j][i+3];
-          K[i+3][j] = P[i+3][j]/temp[j+3][i];
-          K[i+3][j+3] = P[i+3][j+3]/temp[j+3][i+3];
-        }
-    }
-}
+foriunion x; foriunion y; foriunion z;
 
-void KALMAN::measureUpdate(PLANE* plane)
-{
-  Y[0][0] = plane->pAngle[0][0];
-  Y[1][0] = plane->pAngle[2][0];
-  Y[2][0] = plane->pAngle[0][1];
-  Y[3][0] = plane->pAngle[2][1];
-  Y[4][0] = plane->pAngle[0][1];
-  Y[5][0] = plane->pAngle[2][2];
-}
+x.f = longToFeet( plane->loc.z, plane->loc.x) - plane->planeft.wxft;
+y.f = plane->loc.y - plane->planeft.wyft;
+z.f = latToFeet( plane->loc.z) - plane->planeft.wzft;
 
-void KALMAN::updateState()
-{
-  int temp[6];
+float rradius = qsqrt(x.f * x.f + z.f * z.f);
+float rplaneSlope2 = rplaneSlope * rplaneSlope;
+float rradius2 = rradius * rradius;
 
-  for(int i=0; i<3; ++i)
-    {
-      Y[i][0] -= X[i][0];
-      Y[i+3][0] -= X[i+3][0];
+float rx2z2 = qsqrt( px * px + pz * pz);
+float pplaneSlope = py / rx2z2;
+float pplaneSlope2 = pplaneSlope * pplaneSlope;
+float pradius = qsqrt( rradius2 + y.f * y.f);
+float pradius2 = pradius * pradius;
 
-      for(int j=0; j<3; ++j)
-        {
-          temp[i] += (K[i][j] * Y[j][0]) + (K[i][j+3] * Y[j+3][0]);
-          temp[i+3] += (K[i+3][j] * Y[i][0]) + (K[i+3][j+3] * Y[i+3][0]);
+float32x2_t vr = {rradius2, pradius2};
+float32x2_t vs = {rplaneSlope2, pplaneSlope2};
+float32x2_t vh = vld1q_f32(0.5f); /*fill in with the vector fill cmd*/
 
-          X[i][0] = X[i][0] + temp[i];
-          X[i+3][0] = X[i+3][0] + temp[i+3];
-        }
-      
-    }
-}
+vs = vrecps_f32(vs);
+vr = vmul_f32(vr, vs);
+vr = vmul_f32(vr, vh);
 
-void KALMAN::updateP()
-{
-  for(int i=0; i<3; ++i)
-    {
-      for(int j=0; j<3; ++j)
-        {
-          P[i][j] = ((1 - K[i][j]) * P[i][j]);
-          P[i][j+3] = ((1 - K[i][j+3]) * P[i][j+3]);
-          P[i+3][j] = ((1 - K[i+3][j]) * P[i+3][j]);
-          P[i+3][j+3] = ((1 - K[i+3][j+3]) * P[i+3][j+3]);
-        }
-    }
-}
+rradius2 = vget_lane_f32(vr, 0);
+pradius2 = vget_lane_f32(vr, 1);
 
-void KALMAN::pushKalmandData(PLANE* plane)
-{
-  plane->pAngle[0][0] = Y[0][0];
-  plane->pAngle[2][0] = Y[1][0];
-  plane->pAngle[0][1] = Y[2][0];
-  plane->pAngle[2][1] = Y[3][0];
-  plane->pAngle[0][1] = Y[4][0];
-  plane->pAngle[2][2] = Y[5][0];
-}
+foriunion ra; foriunion pa;
+ra.f = qsqrt(rradius2); pa.f = qsqrt(pradius2);
 
-void KALMAN::loop(PLANE* plane)
-{
-  xPred(plane->dt);
-  pPred(plane->dt);
-  kGain();
-  measureUpdate(plane);
-  updateState();
-  updateP(); 
-  pushKalmandData(plane);
+px = ra.f; pz = ra.f * rplaneSlope;
+py = pa.f * pplaneSlope; /*check the mul*/
+
+float rwpSlope = z.f/x.f; float pwpSlope = y.f/rradius;
+
+ra.f *= rwpSlope; pa.f *= pwpSlope;
+ra.f -= px; ra.i &= 0x80000000;
+pa.f -= rx2z2; pa.i &= 0x80000000;
+
+px -= plane->planeft.wxft;
+py -= plane->planeft.wyft;
+pz -= plane->planeft.wzft;
+
+foriunion rerror; foriunion perror;
+rerror.f = qsqrt(px * px + pz * pz) / (2.0f *rradius);
+perror.f = qsqrt(py * py + rradius * rradius) / (2.0f *pradius);
+
+rerror.i |= ra.i ^ (x.i & 0x80000000);
+x.f = rradius; //check this line
+perror.i |= pa.i ^ (x.i & 0x80000000); //check this line too
+
+plane->error.roll = rerror.f * 3.14159f;
+plane->error.pitch = perror.f *3.14159f;
+/*look into weighing the errors*/
 }
