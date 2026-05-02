f06b87127b7470ada5e1e14e83cdce2784794b9c
bobtop42 <nv714774@gmail.com>
2026-05-02 15:14:06 -0600

    Refactor PITCH class constructor and methods

--- pitch.cpp
+++ pitch.cpp
@@ -1,43 +1,55 @@
 #include "pitch.h"
 
-PITCH::PITCH(float targetPitch, float currentPitch, bool engaged): targetPitch_(targetPitch), currentPitch_(currentPitch), engaged_(engaged) {};
+PITCH::PITCH(float targetpitch, float currentpitch, bool engaged):targetPitch_(targetpitch), currentPitch_(currentpitch), engaged_(engaged) {}
 
 void PITCH::engaged()
 {
-  engaged_ = true;
-};
+    engaged_ = true;
+}
 
 void PITCH::disengaged()
 {
-  engaged_ = false;
-};
+    engaged_ = false;
+}
 
 void PITCH::targetPitch(float value, PLANE* plane)
 {
-  targetPitch_ = bfabs(value);
-  PitchDir = static_cast<int8_t>(value/bfabs(value) + static_cast<float>(!static_cast<int>(bfabs(value))));
-  //read target roll for description of this code, its the same logic/value wise, but with pitch instead of roll
-  
+    targetPitch_ = bfabs(value);
+    foriunion pitchPoN; pitchPoN.f = value;
+    PitchDir = (((!(pitchPoN.i & 0x80000000))<<1)-1) * (!!(pitchPoN.i & 0x7FFFFFFF));
+    /*
+    returns -1 for pitching dir left, 0 for on course for pitch, and 1 for pitching dir right.
+    code basicaly  does this:
+    value += 0.9999999; //makes sure any dec >=1, so when truncates non- zero goes to 1
+    static_cast -> int to truncate, then turns all non-zero vals to 0, and 0 -> 1, so when you div, no non-zero errors, then goes back to float, to div then int8_t for pitchDir final value
+    */
 }
 
 void PITCH::targetPitch(PLANE* plane)
 {
-  targetPitch_ = bfabs(plane->WPA.pitch - plane->PA.pitch);
-  PitchDir = (plane->WPA.pitch - plane->PA.pitch)/bfabs(plane->WPA.pitch - plane->PA.pitch);
-};
+    float pa = plane->PA.pitch;
+    float wp = plane->WPA.pitch;
+
+    foriunion pitchPoN; pitchPoN.f = wp - pa;
+    foriunion isbig; isbig.f = bfabs(pitchPoN.f) - 3.141592f;
+    isbig.i = !(isbig.i & 0x80000000);
+    PitchDir = (((!((!(pitchPoN.i & 0x80000000))^(!isbig.i)))<<1)-1)*(!!(pitchPoN.i & 0x7FFFFFFF));
+    isbig.i = (isbig.i * 0x40C90FDB) | (PitchDir & 0x80000000);
+    targetPitch_ = bfabs(isbig.f + pitchPoN.f);
+}
 
 void PITCH::adjustElevator(float value, PLANE* plane)
 {
-  plane->ep.flap.fR = plane->ep.flap.fL += value * static_cast<float>(PitchDir);
-  plane->ep.flapAdj(plane->ep);
+    plane->ap.flap.fR = plane->ap.flap.fL += value * static_cast<float>(PitchDir);
+    plane->ap.flapAdj(plane->ap);
 }
 
 void PITCH::update(PLANE* plane)
 {
-  if(engaged_)
-  {
-    targetPitch(plane);
-    float pidAdj = pid.calculate(targetPitch_);
-    adjustElevator(pidAdj, plane);
-  }
+    if (engaged_)
+    {
+        targetPitch(plane);
+        float pidAdj = pid.calculate(targetPitch_);
+        adjustElevator(pidAdj, plane);
+    }
 }
