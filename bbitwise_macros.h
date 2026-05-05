5b7bf65c52d5d377a60d1bb178ed27711cc03972
bobtop42 <nv714774@gmail.com>
2026-05-05 15:34:10 -0600

    Add bitwise macros for 8, 16, 32, and 64 bits
    Define bitwise macros for various data sizes and type conversions.

---
+++ bbitwise_macros.h
@@ -0,0 +1,69 @@
+#ifndef BBITWISEMACROS_H
+#define BBITWISEMACROS_H
+
+#ifndef signbit64
+#define signbit64 0x8000000000000000
+#endif
+
+#ifndef signbit32
+#define signbit32 0x80000000
+#endif
+
+#ifndef signbit16
+#define signbit16 0x8000
+#endif
+
+#ifndef signbit8
+#define signbit8 0x80
+#endif
+
+#ifndef data64
+#define data64 0x7FFFFFFFFFFFFFFF
+#endif
+
+#ifndef data32
+#define data32 0x7FFFFFFF
+#endif
+
+#ifndef data16
+#define data16 0x7FFF
+#endif
+
+#ifndef data8
+#define data8 0x7F
+#endif
+
+//reg manipulation
+#ifndef fti64
+#define fti64 *(int64_t*)&
+#endif
+
+#ifndef fti32
+#define fti32 *(int32_t*)&
+#endif
+
+#ifndef fti16
+#define fti16 *(int16_t*)&
+#endif
+
+#ifndef fti8
+#define fti8 *(int8_t*)&
+#endif
+
+#ifndef itf64
+#define itf64 *(double*)&
+#endif
+
+#ifndef itf32
+#define itf32 *(float*)&
+#endif
+
+#ifndef itf16
+#define itf16 *(float*)&
+#endif
+
+#ifndef itf8
+#define itf8 *(float*)&
+#endif
+
+#endif
