5ea5056be528c7e82a2cb869b03a1c1ed3e99c2b
bobtop42 <nv714774@gmail.com>
2026-06-05 16:58:53 -0600

    Add is_same templates and bit_len declaration

--- blib/bstd.h
+++ blib/bstd.h
@@ -8,4 +8,13 @@
 namespace bstd
 {
   int32_t stoi(char *chars, int32_t *index);
+
+  template<typename T>
+  bool is_same(T a, T b){return true;};
+
+  template<typename T, typename U>
+  bool is_same(T a, U b){return false;};
+
+  template<typename T>
+  int32_t bit_len(T type);
 };
